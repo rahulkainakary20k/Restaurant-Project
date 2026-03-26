50ca05b58fc4e420d2310adc2fd1a90ba7dcbe44
rahulkainakary20k <kainakaryrahul@gmail.com>
2026-03-26 17:04:34 +0530

    Added backend root route

--- Backend/server.js
+++ Backend/server.js
@@ -20,10 +20,13 @@ app.use ('/api/menu',menuRouter)
 app.use ('/api/orders',orderRouter)
 app.use ('/api/bills',billRouter)
 app.use("/api/pay", payRouter)
+
+// in using to render platform in live deploy stage 
  app.get ('/',(req,res)=> {
     res.send ( "Back-end side is Running...!!!" )
  })
 
+ 
 const mongoURL = process.env.MONGO_URL
 console.log('Connecting to MongoDB at', mongoURL)
 
